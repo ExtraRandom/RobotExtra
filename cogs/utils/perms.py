9f42fbf387162ab93c36f3c60ee8413d82ff432f
Extra_Random <crazyblob11@gmail.com>
2021-07-22 20:16:40 +0100

    Updated permission checks

--- cogs/utils/perms.py
+++ cogs/utils/perms.py
@@ -5,15 +5,22 @@ from cogs.utils import errors
 
 def is_server_owner():
     def predicate(ctx):
-        return ctx.message.author.id == ctx.message.channel.server.owner
-
+        return ctx.message.author.id == ctx.message.channel.guild.owner
     return commands.check(predicate)
 
 
 def is_dev():
     def predicate(ctx):
         return ctx.message.author.id == 92562410493202432
+    return commands.check(predicate)
 
+
+def is_server_owner_or_dev():
+    def predicate(ctx):
+        if ctx.message.author.id == ctx.message.channel.guild.owner or ctx.message.author.id == 92562410493202432:
+            return True
+        else:
+            return False
     return commands.check(predicate)
 
 
@@ -23,75 +30,10 @@ def is_in_somewhere_nice():
             return True
         else:
             raise errors.WrongGuild("Command cannot be used in this server")
-
     return commands.check(predicate)
 
 
 def is_admin():
     def predicate(ctx):
-        if ctx.message.guild.id == 750689226382901288:  # sn
-            return ctx.message.author.id in [735446137489719307, 750341810873696316,
-                                             92562410493202432, 309514963037192204]
-        elif ctx.message.guild.id == 863589037959938098:
-            return ctx.message.author.id in [287420218651967518, 92562410493202432]  # utms
-        else:
-            if ctx.message.author.id == 92562410493202432:
-                return True
-
-    return commands.check(predicate)
-
-
-def is_admin_or_mod():  # not used
-    def predicate(ctx):
-        return ctx.message.author.id in [735446137489719307, 750341810873696316, 92562410493202432, 309514963037192204]
-
-    return commands.check(predicate)
-
-# TODO see what is actually useful out of the stuff below and remove whatever isn't
-
-
-"""
-taken from
-https://github.com/ExtraRandom/Discord-Bot/blob/2aee29b05becd612757197d76777e05b807cacb6/helpers/checks.py
-"""
-
-
-def is_owner_check(message):
-    return message.author.id == message.channel.server.owner
-
-
-def check_permissions(ctx, perms):
-    msg = ctx.message
-    if is_owner_check(msg):
-        return True
-
-    ch = msg.channel
-    author = msg.author
-    resolved = ch.permissions_for(author)
-    return all(getattr(resolved, name, None) == value for name, value in perms.items())
-
-
-def role_or_permissions(ctx, check, **perms):
-    if check_permissions(ctx, perms):
-        return True
-
-    ch = ctx.message.channel
-    author = ctx.message.author
-    if ch.is_private:
-        return False  # can't have roles in PMs
-
-    role = discord.utils.find(check, author.roles)
-    return role is not None
-
-
-def mod_or_permissions(**perms):
-    def predicate(ctx):
-        return role_or_permissions(ctx, lambda r: r.name in ('Epic', 'Legendary'), **perms)
-
-    return commands.check(predicate)
-
-
-def admin_or_permissions(**perms):
-    def predicate(ctx):
-        return role_or_permissions(ctx, lambda r: r.name == 'Bot Admin', **perms)
+        return ctx.message.author.guild_permissions.administrator
     return commands.check(predicate)
