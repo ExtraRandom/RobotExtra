692ed8dab33fc06109d35a8d5f7f3f559040421e
ExtraRandom <ExtraRandom@users.noreply.github.com>
2024-03-19 03:39:16 +0000

    Updated IsThereAnyDeal command to use the new API

--- cogs/games.py
+++ cogs/games.py
@@ -14,9 +14,99 @@ class Games(commands.Cog):
         self.bot = bot
 
     @commands.slash_command()
-    async def itad(self, ctx, search_term: discord.Option(str, "The term to search for", required=True)):
+    async def itad(self, ctx,
+                   search_term: discord.Option(str, description="The term to search for", required=True)#,
+                   # #allow_vouchers: discord.Option(bool,
+                   #                               description="Allow Vouchers when considering cheapest price",
+                   #                               required=False, default=True)
+                   ):
+        """IsThereAnyDeal.com Search"""
+
+        await ctx.defer()
+
+        allow_vouchers = True  # todo decide what to do with this
+
+        key = IO.fetch_from_settings('keys', 'itad_api')
+        if key is None:
+            # TODO write to log (maybe?)
+            await ctx.respond("No ITAD API key in settings config.", delete_after=15)
+            return
+
+        search = parse.quote(search_term)
+        limit = 10
+        result_limit = 4
+        search_url = f"https://api.isthereanydeal.com/games/search/v1?key={key}&title={search}&results={limit}"
+
+        res = requests.get(search_url)
+
+        results = {}
+        overview_payload = []
+
+        if res is not None and res.status_code == 200:
+            # print(res.text)
+            search_json = res.json()
+
+            for result in search_json:
+                results[result['id']] = result['title']
+
+                overview_payload.append(result['id'])
+        else:
+            await ctx.respond("An error occurred whilst searching for games")
+            return
+
+        # print(results)
+        # print(overview_payload)
+
+        country = "GB"  # Two-letter country code (ISO 3166-1 alpha-2)
+
+        overview_url = (f"https://api.isthereanydeal.com/games/overview/v2"
+                        f"?key={key}&country={country}&voucher={allow_vouchers}")
+        overview_res = requests.post(overview_url, json=overview_payload)
+
+        embed = discord.Embed(title=f"IsThereAnyDeal.com Search for '{search_term}'", colour=discord.Colour.blue())
+
+        if overview_res is not None and overview_res.status_code == 200:
+            overview_data = overview_res.json()['prices']
+            # print(overview_res.text)
+
+            for prices in overview_data:
+
+                price_raw = prices['current']['price']['amount']
+                if price_raw == 0:
+                    price_clean = "Free"
+                else:
+                    price_clean = f"£{price_raw}"
+
+                voucher = ""
+                if prices['current']['voucher']:
+                    voucher = f"with code **{prices['current']['voucher']}**"
+
+                embed.add_field(name=f"{results[prices['id']]}",
+                                value=f"Price: **{price_clean}** {voucher}\n"
+                                      f"Store: **[{prices['current']['shop']['name']}]({prices['current']['url']})**\n"
+                                      f"Info: **[All Prices]({prices['urls']['game']})**")
+
+                if len(embed.fields) >= result_limit:
+                    break
+        else:
+            await ctx.respond("An error occurred whilst fetching game prices")
+            return
+
+        embed.set_footer(text="This is a reworked version of the ITAD command using the new API, "
+                              "it may still have some bugs!")
+        await ctx.respond(embed=embed)
+
+    @commands.slash_command(guild_ids=[223132558609612810])
+    async def itad_old(self, ctx,
+                   search_term: discord.Option(str, "The term to search for", required=True)
+                   ):
         """IsThereAnyDeal.com Search"""
         await ctx.defer()
+
+        # await ctx.respond("Command temporarily disabled whilst it is being updated for the new ITAD API. Sorry.")
+        # return
+
+        use_new_api = True
 
         key = IO.fetch_from_settings('keys', 'itad_api')
         if key is None:
@@ -24,20 +114,34 @@ class Games(commands.Cog):
             return
 
         search = parse.quote(search_term)
-
         limit = 4
-        search_url = "https://api.isthereanydeal.com/v02/search/search/?key={}&q={}&limit={}&strict=0" \
-                     "".format(key, search, limit)
+
+        # TODO - NEW API REQUIRES TESTING WHEN IT IS LIVE
+        # API DOCS - https://docs.isthereanydeal.com/#tag/games/operation/games-overview-v2
+        # MIGRATION GUIDE - https://github.com/IsThereAnyDeal/API/blob/draft/migration.md
+
+        if use_new_api == True:  # TODO TEST NEW API
+            search_url = f"https://api.isthereanydeal.com/games/search/v1?key={key}&title={search}&results={limit}"
+        else:
+            search_url = ("https://api.isthereanydeal.com/v02/search/search/?key={}&q={}&limit={}&strict=0"
+                          "").format(key, search, limit)
 
         res = requests.get(search_url)
         res_code = res.status_code
         title_lookup = {}
+
         if res is not None and res_code == 200:
             plains_data = res.json()
             plains_list = []
+
             for result in plains_data['data']['results']:
-                plains_list.append(result['plain'])
-                title_lookup[result['plain']] = result['title']
+                if use_new_api == True:  # TODO TEST NEW API
+                    plains_list.append(result['id'])  # Plains wont be used on the new API, ID used instead
+                    # change "plains" to "id" later
+                    title_lookup[result['id']] = result['title']
+                else:
+                    plains_list.append(result['plain'])
+                    title_lookup[result['plain']] = result['title']
         else:
             await ctx.respond("ITAD Search Failed, See logs for more info")
             Logger.write(f"--------------\nITAD Plain Search Failed\nURL: {search_url}\nStatus Code: {res_code}"
@@ -48,8 +152,16 @@ class Games(commands.Cog):
             await ctx.respond(f"No games with title '{search_term}' found. Try a different search term.")
             return
 
+
+        if use_new_api == True:
+            overview_url = f"https://api.isthereanydeal.com/games/overview/v2/?key={key}&country=GB"
+
+
         overview_url = "https://api.isthereanydeal.com/v01/game/overview/?key={}&region=uk&country=GB" \
                        "&plains={}".format(key, ",".join(plains_list))
+
+
+
         overview_res = requests.get(overview_url)
         overview_res_code = overview_res.status_code
 
